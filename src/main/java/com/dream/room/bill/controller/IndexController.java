b6192f9ddaa98136dee411eb31e8704da22c3d71
jiangtj <116749895@qq.com>
2018-02-08 17:11:14 +0800

    Add

--- src/main/java/com/dream/room/bill/controller/IndexController.java
+++ src/main/java/com/dream/room/bill/controller/IndexController.java
@@ -1,11 +1,13 @@
 package com.dream.room.bill.controller;
 
+import com.dream.room.bill.entity.User;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -32,14 +34,28 @@ public class IndexController {
     }
 
     @PostMapping("/login/rest")
-    public String login(String no,String password){
-        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(no,password);
+    public String login(@RequestBody User user){
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getNo(),user.getPassword());
         Authentication authenticate = authenticationManager.authenticate(token);
         if (authenticate.isAuthenticated()){
             SecurityContextHolder.getContext().setAuthentication(authenticate);
             return "success";
         }
         return "fail";
+    }
+
+    @PostMapping("/login/principal")
+    public Map<String,Object> check(){
+        Map<String,Object> map = new HashMap<>();
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if ("anonymousUser".equals(principal)){
+            map.put("authentication","false");
+            map.put("principal","anonymousUser");
+            return map;
+        }
+        map.put("authentication","true");
+        map.put("principal", principal);
+        return map;
     }
 
 }
